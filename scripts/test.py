1af2b2c5b2f432b41e7a8a7589413e8a4250a071
Gerhard Gruber <155916886+ggruber193@users.noreply.github.com>
2025-04-19 10:55:38 +0200

    Create test.py

---
+++ scripts/test.py
@@ -0,0 +1,17 @@
+from pathlib import Path
+import argparse
+
+
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('--directory', type=Path)
+  parser.add_argument(--changed-files, type=Path, nargs="*")
+
+  args = parser.parse_args()
+
+  directory = args.directory
+  changed_files = args.changed_files
+
+  print(directory)
+  print(list(directory.rglob("*")))
+  print(changed_files)
