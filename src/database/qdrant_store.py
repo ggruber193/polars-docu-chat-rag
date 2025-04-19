f150aff4a6c6f3fadf6af197ac925867ea33e37f
Gerhard <gerhard-richard.gruber@outlook.at>
2025-04-19 23:22:54 +0200

    implement the rag app

--- src/database/qdrant_store.py
+++ src/database/qdrant_store.py
@@ -2,8 +2,10 @@ from typing import Any
 
 from qdrant_client import QdrantClient, models
 from uuid import uuid4
+from transformers import PreTrainedModel
 
-from src.config import QDRANT_COLLECTION_NAME
+from src.config import QDRANT_COLLECTION_NAME, QDRANT_URL, QDRANT_API_KEY, EMBEDDING_MODEL
+from src.embeddings import TextEmbedder
 
 
 class QdrantStore:
@@ -59,3 +61,23 @@ class QdrantStore:
             values = [values]
         filter = {"must": [models.FieldCondition(key=key, match=models.MatchAny(any=values))]}
         self.delete_points(filter, collection_name)
+
+    def get_topk_points_single(self,
+                               query: Any | str,
+                               collection_name: str,
+                               k=5):
+        responses = self.client.query_points(collection_name=collection_name,
+                                             query=query,
+                                             limit=k)
+
+        return [i.payload["text"] for i in responses.points]
+
+
+if __name__ == '__main__':
+    client = QdrantClient(QDRANT_URL, api_key=QDRANT_API_KEY)
+    qdrant_store = QdrantStore(client)
+    embedding_model = TextEmbedder(modelname=EMBEDDING_MODEL)
+    query = "How to filter a dataframe"
+    query_emb = embedding_model.embed_text(query)
+    responses = qdrant_store.get_topk_points_single(query_emb[0], collection_name=QDRANT_COLLECTION_NAME)
+    print(responses)
---
+++ src/retrieval/retrieval.py
@@ -0,0 +1,6 @@
+
+from src.database.qdrant_store import QdrantStore
+from src.embeddings import TextEmbedder
+
+def embed_query(query: str | list[str], ):
+    pass
